9a965129544f8f3904b628ba8dc1692ec22e4865
Jonathan Terrell <terrell.jm@gmail.com>
2026-04-27 18:54:32 +0200

    v0.3.653

--- dist/types/src/component/connection/index.d.ts
+++ dist/types/src/component/connection/index.d.ts
@@ -31,7 +31,7 @@ export interface ConnectionNodeConfig {
     extension: string | undefined;
     folderPath: string;
     handle: DPAFileSystemFileHandle | undefined;
-    icon?: string;
+    icon?: unknown;
     id: string;
     label: string;
     lastModifiedAt: number | undefined;
