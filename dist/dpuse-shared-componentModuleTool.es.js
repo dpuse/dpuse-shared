98aa7a57fea1c08174079208f615ef73950d8ff2
Jonathan Terrell <terrell.jm@gmail.com>
2026-04-27 18:55:21 +0200

    v0.3.654

--- dist/dpuse-shared-componentModuleTool.es.js
+++ dist/dpuse-shared-componentModuleTool.es.js
@@ -3,7 +3,10 @@ var e = "https://engine-eu.dpuse.app/tools";
 async function t(t, n) {
 	let r = `dpuse-tool-${n}`, i = t.find((e) => e.id === r);
 	if (!i) throw Error(`Connector could not load unknown tool '${n}'.`);
-	return new (await (import(`${e}/${n}_v${i.version}/${r}.es.js`))).Tool();
+	return new (await (import(
+		/* @vite-ignore */
+		`${e}/${n}_v${i.version}/${r}.es.js`
+))).Tool();
 }
 //#endregion
 export { t as loadTool };
