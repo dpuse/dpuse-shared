5b3e1dddbf498984b55ea0691a6b65c3833f32d8
Jonathan Terrell <terrell.jm@gmail.com>
2026-07-03 14:43:28 +0200

    v0.3.729

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import Sonda from 'sonda/vite';
-import { visualizer } from 'rollup-plugin-visualizer';
 import { fileURLToPath, URL } from 'node:url';
 
 // ── Data
@@ -44,10 +43,7 @@ export default defineConfig({
             formats: ['es']
         },
         rollupOptions: {
-            plugins: [
-                Sonda({ filename: 'index', format: 'html', gzip: true, brotli: true, open: false, outputDir: './bundle-analysis-reports/sonda' }),
-                visualizer({ filename: './bundle-analysis-reports/rollup-visualiser/index.html', open: false, gzipSize: true, brotliSize: true })
-            ]
+            plugins: [Sonda({ filename: 'index', format: 'json', brotli: true, gzip: false, open: false, outputDir: './bundle-analysis-reports/sonda' })]
         },
         target: 'ESNext'
     },
